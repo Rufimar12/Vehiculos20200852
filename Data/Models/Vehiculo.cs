066e728f27b026df3cbbaed0460136991a885645
Rufimar12 <rufi0121.euphoria@gmail.com>
2022-11-04 11:40:16 -0400

    first commit

---
+++ Data/Models/Vehiculo.cs
@@ -0,0 +1,9 @@
+namespace Vehiculos20200852.Data.Models;
+
+public class Vehiculo{
+    public int VehiculoID { get; set; }
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+    public int Año { get; set; }
+    public int Color { get; set; }
+}
